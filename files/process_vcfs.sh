806265c8e9bd13f5e46527a69348ae21c94e2a00
prokpi <prokopimaite@gmail.com>
2025-05-26 16:20:29 +0200

    Update process_vcfs.sh

--- files/process_vcfs.sh
+++ files/process_vcfs.sh
@@ -10,9 +10,6 @@
 source /shared/conda/miniconda3/etc/profile.d/conda.sh
 conda activate pandas
  
-echo "Using Python from: $(which python)"
-echo "Python version: $(python --version)"
-echo "Pandas version: $(python -c 'import pandas as pd; print(pd.__version__)')"
  
 INPUT_DIR="/shared/work/PI-tommaso.pippucci/RF-WGS/SVs/results/concatenatingfiles/vcf_files"
 OUTPUT_FOLDER="/shared/work/PI-tommaso.pippucci/RF-WGS/SVs/results/concatenatingfiles/output"
@@ -24,7 +21,7 @@ for VCF_PATH in "$INPUT_DIR"/*_merged.vcf; do
         SAMPLE_NAME=$(basename "$VCF_PATH" "_merged.vcf")
         OUTPUT_PATH="$OUTPUT_FOLDER/${SAMPLE_NAME}_processed.tsv"
         echo "Processing $VCF_PATH -> $OUTPUT_PATH"
-        python3 conc.py --repeat_masker repeat_masker.tsv \
+        python3 process_vcfs.py --repeat_masker repeat_masker.tsv \
                         --duplications segmental_duplications.tsv \
                         --vcf_file "$VCF_PATH" \
                         --output_file "$OUTPUT_PATH"
