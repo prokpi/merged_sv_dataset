7583ecfd356d4602e93f6caea45fca8c34e11657
prokpi <prokopimaite@gmail.com>
2025-06-10 15:05:20 +0200

    Create process_vcfs.sh

---
+++ nextflow/process_vcfs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --mem=16G
+#SBATCH --error="job-%j.err"
+#SBATCH --output="job-%j.out"
+#SBATCH --partition=prod
+#SBATCH --job-name="process_vcfs"
+
+source /shared/conda/miniconda3/etc/profile.d/conda.sh
+conda activate pandas
+
+INPUT_DIR="/shared/work/PI-tommaso.pippucci/RF-WGS/SVs/results/concatenatingfiles/vcf_files"
+OUTPUT_FOLDER="/shared/work/PI-tommaso.pippucci/RF-WGS/SVs/results/concatenatingfiles/output"
+
+mkdir -p "$OUTPUT_FOLDER"
+
+for VCF_PATH in "$INPUT_DIR"/*_merged.vcf; do
+        SAMPLE_NAME=$(basename "$VCF_PATH" "_merged.vcf")
+        OUTPUT_PATH="$OUTPUT_FOLDER/${SAMPLE_NAME}_processed.tsv"
+
+        python3 process_vcfs.py --repeat_masker repeat_masker.tsv \
+                        --duplications segmental_duplications.tsv \
+                        --vcf_file "$VCF_PATH" \
+                        --output_file "$OUTPUT_PATH"
+
+    fi
+done
